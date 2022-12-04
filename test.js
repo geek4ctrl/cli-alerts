e1dc0316e4e9339f239f1465ce7cdc050a662e73
Laurent Mbuyu <laurentmbuyu@Laurents-MacBook-Air.local>
2022-12-04 19:32:56 +0200

    First version

---
+++ test.js
@@ -0,0 +1,9 @@
+const alert = require('./index.js');
+
+alert()
+
+alert({ type: `success`, msg: `Everything finished!`, name: `DONE`});
+alert({ type: `warning`, msg: `You didn't add something!`});
+alert({ type: `info`, msg: `Laurent is awesome!`});
+alert({ type: `error`, msg: `Something went wrong!`});
+
